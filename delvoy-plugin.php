7a2133713f9d939a6b11b5591ccfbeb49a704074
Mateusz Czech <czechmateusz88@gmail.com>
2023-03-14 06:00:37 +0100

    plugin settings page

--- delvoy-plugin.php
+++ delvoy-plugin.php
@@ -17,12 +17,17 @@ defined ('ABSPATH') or die();
 
 class DeLvoyPlugin{ 
 
+    public $plugin_title;
+
     function __construct(){
+        $this->plugin_title = plugin_basename(__FILE__);
         $this->create_post_type();
     }
 
     function register(){
         add_action('admin_enqueue_scripts', array($this, 'enqueue'));
+        add_action('admin_menu', array($this, 'add_admin_pages'));
+        add_filter("plugin_action_links_$this->plugin_title", array($this, 'settings_link'));
     }
 
     protected function create_post_type(){
@@ -40,6 +45,20 @@ class DeLvoyPlugin{
         //enqueue all scripts
         wp_enqueue_style('plugin-style', plugins_url('/assets/plug-styles.css', __FILE__));
         wp_enqueue_script('plugin-script', plugins_url('/assets/plug-scripts.js', __FILE__));
+    }
+
+    public function add_admin_pages(){
+        add_menu_page( 'DeLvoy plugin', 'DeLvoy', 'manage_options', 'delvoy_plugin', array($this, 'admin_index'), 'dashicons-store', 110);
+    }
+
+    public function admin_index(){
+        require_once plugin_dir_path(__FILE__) . 'templates/admin_page.php';
+    }
+
+    public function settings_link($links){
+        $settings_link = '<a href="admin.php?page=delvoy_plugin">Settings</a>';
+        array_push($links, $settings_link);
+        return $links;
     }
 
     function activate(){
---
+++ templates/admin_page.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+    @package DelvoyPlugin
+*/  
+
+?>
+
+<h1>DeLvoy plugin settings</h1>
